1c0e6c3f0fb2ca341ffa435d60389031a9038eac
Ash Hoskins <s559245@nwmissouri.edu>
2023-01-19 03:40:20 -0600

    Module 2 Task 3

--- user_math.py
+++ user_math.py
@@ -1,15 +1,13 @@
 """
-Always start with a docstring that describes what the module does.
-Include your name and the date.
+Student Name: Ash Hoskins, Student #S559245
+Course: CSIS 44-609 - Data Analytics Fundamentals
+Professor Denise Case
+Module 2: Assignment 1 Task 3.
 
-Use built-in functions and 
-functions from the math module.
-
-To illustrate the ability to call functions and 
-display useful results to the user. 
-
-Use your textbook and the examples in this repo to get ideas.
-
+This script calculates the area of a lot, utilzies math libraries for comb and perm with
+the given values in the assignment. The 3 custom functions 1.) converts longitude and latitude to radians
+2.) Utilizes the haversine formula to calculate the great spherical distances between the loscar volcano and
+Northwest Missouri State University Campus. 3.) Converts haversine results from meters to miles.
 """
 
 import math
@@ -27,7 +25,7 @@ def get_area_of_lot(length, width):
     # Use a try / except / finally block when something 
     # could go wrong
     try: 
-        area = 0 # fix this
+        area = length * width # Corrected for Task 3, Step 6 (Part A)
         return area
     except Exception as ex:
         print(f"Error: {ex}")
@@ -35,7 +33,43 @@ def get_area_of_lot(length, width):
 
 
 # define more functions here (see instuctions)
+# Function to convert longitude and latitude Angles to radians
+def DtoR(long, lat):
+    try:
+        lo = math.radians(long)
+        la = math.radians(lat)
+        return lo, la
+    except Exception as ex:
+        print(f"Error: {ex}")
+        return None
+# Haversine formula for calculating great-circle distance between two points on a sphere.
+# Calculates the shortest distance over the Earth's Surface following a great-circle path.
+def Haversine(long2, lat2) :
+    try:
+        r = round(63710000/10)
+        nwmslong = 40.3520
+        nwmslat = 94.8825
+        
+        theta1 = lat2 * math.pi/180
+        theta2  = nwmslat * math.pi/180
+        alphaphi = (lat2 - nwmslat) * math.pi/180
+        alphalambda = (long2 - nwmslong) * math.pi/180
 
+        a = math.sin(alphaphi/2) * math.sin(alphaphi/2) + math.cos(theta1) * math.cos(theta2) * math.sin(alphalambda/2) * math.sin(alphalambda/2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        Distance = r * c
+        return Distance   
+    except Exception as ex:
+        print(f"Error: {ex}")
+        return None
+#Simply converts meters to miles.       
+def meterstomiles(meters) :
+    try:
+        miles = meters * .00062137
+        return miles
+    except Exception as ex:
+        print(f"Error: {ex}")
+        return None
 
 
 
@@ -49,4 +83,18 @@ def get_area_of_lot(length, width):
 if __name__ == "__main__":
 
     # call your functions here (see instructions)
-    print("your code here")
+    print("Explore some functions in the math module")
+    print()
+    print(f"math.comb(5,1) = {math.comb(5,1)}")
+    print(f"math.perm(5,1) = {math.perm(5,1)}")
+    print()
+
+    #Printing and calling functions within the domain for the Module assignment.
+    print("The Location of the Lascar Volcano in Chile is: 23.37 degrees south,  67.73 degrees west")
+    print("The Coordinates in radians is: ", DtoR(23.37,67.73))
+    #Places haversine results into variable for miles converstion
+    zx = round(Haversine(23.37, 67.73),)
+    print("The Great Spherical Distance of The Lascar Volcano to NWMS University is: ",zx, " Meters or",round(meterstomiles(zx), 2), " Miles" )
+
+    #Module 2, Task 3, Step 2 (Part B) - Calling method/function with values 6,2
+    print("The Area of the Lot is: ", get_area_of_lot(6,2))
---
+++ user_mathout.py
@@ -0,0 +1,20 @@
+"""
+Student Name: Ash Hoskins, Student #S559245
+Course: CSIS 44-609 - Data Analytics Fundamentals
+Professor Denise Case
+Module 2: Assignment 1 Task 3.
+
+Results from Terminal for user_math.py
+
+PS C:\Users\Hoski\Desktop\NWMS\GIT\datafun-02-functions\datafun-02-functions> & C:/Users/Hoski/miniconda3/python.exe c:/Users/Hoski/Desktop/NWMS/GIT/datafun-02-functions/datafun-02-functions/user_math.py
+Explore some functions in the math module
+
+math.comb(5,1) = 5
+math.perm(5,1) = 5
+
+The Location of the Lascar Volcano in Chile is: 23.37 degrees south,  67.73 degrees west
+The Coordinates in radians is:  (0.4078834461910748, 1.1821115023757596)
+The Great Spherical Distance of The Lascar Volcano to NWMS University is:  2999526  Meters or 1863.82  Miles
+The Area of the Lot is:  12
+PS C:\Users\Hoski\Desktop\NWMS\GIT\datafun-02-functions\datafun-02-functions>
+"""
