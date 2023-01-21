21a9a5af5f07ecce4176f68f75a1136eec3bfc65
Ash Hoskins <s559245@nwmissouri.edu>
2023-01-21 04:51:42 -0600

    Module 2 Task 7 Xtra

--- xtra_p2.py
+++ xtra_p2.py
@@ -1,4 +1,15 @@
 """
+Student Name: Ash Hoskins, Student #S559245
+Course: CSIS 44-609 - Data Analytics Fundamentals
+Professor Denise Case
+Domain: Geospatial data
+Module 2: Assignment 1 Task 7: Xtra.
+
+This script utilizes the doctest library by testing the methods
+with predefined arguments. The purpose of this assignment was to fix the functions
+so they work properly with the doctest below and stress test the funciontality. 
+
+
 Optional bonus. See course site for details.
 
 
@@ -30,7 +41,7 @@ import doctest
 # define some existing functions
 def add_two(first, second):
     """Return the sum of any two arguments."""
-    sum = first   # fix this line
+    sum = first + second  # Sums two variables
     return sum
 
 
@@ -38,7 +49,7 @@ def add_triangle_list(list_triangle):
     """Return the sum of three numbers in a list."""
     sum = 0
     for value in list_triangle:
-        sum = sum + 0  # fix this line to add the value instead of 0
+        sum += +value  # fixed to add the value to the sum
     return sum
 
 
@@ -46,7 +57,7 @@ def add_any(*args):
     """Return the sum of numbers, using built-in *args."""
     sum = 0
     for x in args:
-        sum += 1  # fix this line to add x instead of 1
+        sum += +x  # Fixed to add X to the sum of the arguments
     return sum
 
 
@@ -54,41 +65,18 @@ def add_any_with_keywords(**kwargs):
     """Return the sum of numbers, using built-in keyword args, **kwargs."""
     sum = 0
     for value in kwargs.values():  # use values() - name doesn't matter
-        sum += value  # Use the popular and concise version of sum = sum + x
+        sum += +value  # add the value of each key in the kwargs.
     return sum
 
 
 # TODO: implment a new function to convert celsius to fahrenheit
 # Use round as needed to make the test pass
 # The name of the function is provided in the docstring above
-
-
-
-
-
-
-
-
+# Converts Celcius to Fahrenheit
+def convert_ctof(i):
+    temp = (i * 1.8) + 32
+    return temp
 
 
 if __name__ == "__main__":
-
-
-    print("===========================================================")
-    print("Running doctest.testmod() function to unit test our code")
-    print("===========================================================")
-    print()
     doctest.testmod()
-    print()
-    print("===========================================================")
-    print("If you don't see any results, all tests passed.")
-    print("===========================================================")
-    print("Run with the -v flag to show results regardless.")
-    print("Hit up arrow (to get last command) and add space -v")
-    print("===========================================================")
-    print()
-    print("Read error messages carefully.")
-    print("Errors tell which line number has the error")
-    print("and what the issue is. Scroll up in the terminal to see.")
-    print("Fix issues one at a time until you get the behavior")
-    print("Described in the docstring.")
